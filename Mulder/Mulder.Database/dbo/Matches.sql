de0e1efa69fe3c8e0f835a3d60215c9a99e5f0f3
Piotr Sobkowski <piotr.sobkowski@concent.no>
2018-09-14 17:31:06 +0200

    Db model updated

--- Mulder/Mulder.Database/dbo/Matches.sql
+++ Mulder/Mulder.Database/dbo/Matches.sql
@@ -2,5 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[Location] NVARCHAR(20) NOT NULL,
+	[Address] NVARCHAR(255) NOT NULL,
 	[Date] DateTime,
+	[Year] SMALLINT,
 )
--- Mulder/Mulder.Database/dbo/Players.sql
+++ Mulder/Mulder.Database/dbo/Players.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Players]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[CurrentTeamId] INT NOT NULL,
 	[Name] NVARCHAR(20) NOT NULL,
 	[NickName] NVARCHAR(10) NULL,
-	[Number] NVARCHAR(4) NOT NULL,
-	[CurrentTeamId] INT NOT NULL,
+	[Number] NVARCHAR(4) NULL,
 	[OnLoan] BIT,
 	CONSTRAINT [FK_Players_Teams] FOREIGN KEY ([CurrentTeamId]) REFERENCES [dbo].Teams([Id])
 )
